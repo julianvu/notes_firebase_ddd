d627fba2694bf18d1accfa3b57ce0095f3258638
Julian Vu <julian@uniplanteam.com>
2021-01-26 20:53:53 -0800

    Add Notes value objects

---
+++ lib/domain/core/value_transformers.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+/// Returns the [color] with full opacity.
+Color makeColorOpaque(Color color) {
+  return color.withOpacity(1);
+}
---
+++ lib/domain/notes/value_objects.dart
@@ -0,0 +1,88 @@
+import 'dart:ui';
+
+import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:notes_firebase_ddd_course/domain/core/failures.dart';
+import 'package:notes_firebase_ddd_course/domain/core/value_objects.dart';
+import 'package:notes_firebase_ddd_course/domain/core/value_transformers.dart';
+import 'package:notes_firebase_ddd_course/domain/core/value_validators.dart';
+
+class NoteBody extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 1000;
+
+  factory NoteBody(String input) {
+    assert(input != null);
+    return NoteBody._(
+      validateMaxStringLength(input, maxLength).flatMap(validateStringNotEmpty),
+    );
+  }
+
+  const NoteBody._(this.value);
+}
+
+class TodoName extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 30;
+
+  factory TodoName(String input) {
+    assert(input != null);
+    return TodoName._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(validateStringNotEmpty)
+          .flatMap(validateSingleLine),
+    );
+  }
+
+  const TodoName._(this.value);
+}
+
+class NoteColor extends ValueObject<Color> {
+  static const List<Color> predefinedColors = [
+    Color(0xfffafafa), // Canvas
+    Color(0xfffa8072), // Salmon
+    Color(0xfffedc56), // Mustard
+    Color(0xffd0f0c0), // Tea
+    Color(0xfffca3b7), // Flamingo
+    Color(0xff997950), // Tortilla
+    Color(0xfffffdd0), // Cream
+  ];
+
+  @override
+  final Either<ValueFailure<Color>, Color> value;
+
+  factory NoteColor(Color input) {
+    assert(input != null);
+    return NoteColor._(
+      right(
+        makeColorOpaque(input),
+      ),
+    );
+  }
+
+  const NoteColor._(this.value);
+}
+
+class List3<T> extends ValueObject<KtList<T>> {
+  @override
+  final Either<ValueFailure<KtList<T>>, KtList<T>> value;
+
+  static const maxLength = 3;
+
+  factory List3(KtList<T> input) {
+    assert(input != null);
+    return List3._(
+      validateMaxListLength(input, maxLength),
+    );
+  }
+
+  const List3._(this.value);
+
+  int get length {
+    return value.getOrElse(() => emptyList()).size;
+  }
+}
