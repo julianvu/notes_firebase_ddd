3cc5153a89d0bb0a07a8238345364e80daeb7733
Julian Vu <julian@uniplanteam.com>
2021-02-03 15:40:08 -0800

    Add Todo creation ListTile

--- lib/presentation/notes/note_form/note_form_page.dart
+++ lib/presentation/notes/note_form/note_form_page.dart
@@ -6,9 +6,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_firebase_ddd_course/application/notes/note_form/note_form_bloc.dart';
 import 'package:notes_firebase_ddd_course/domain/notes/note.dart';
 import 'package:notes_firebase_ddd_course/injection.dart';
+import 'package:notes_firebase_ddd_course/presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
+import 'package:notes_firebase_ddd_course/presentation/notes/note_form/widgets/add_todo_tile_widget.dart';
 import 'package:notes_firebase_ddd_course/presentation/notes/note_form/widgets/body_field_widget.dart';
 import 'package:notes_firebase_ddd_course/presentation/notes/note_form/widgets/color_field_widget.dart';
 import 'package:notes_firebase_ddd_course/presentation/routes/router.gr.dart';
+import 'package:provider/provider.dart';
 
 class NoteFormPage extends StatelessWidget {
   final Note editedNote;
@@ -136,16 +139,21 @@ class NoteFormPageScaffold extends StatelessWidget {
         buildWhen: (previous, current) =>
             previous.showErrorMessages != current.showErrorMessages,
         builder: (context, state) {
-          return Form(
-              autovalidateMode: state.showErrorMessages,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const BodyField(),
-                    const ColorField(),
-                  ],
-                ),
-              ));
+          return ChangeNotifierProvider(
+            create: (context) => FormTodos(),
+            child: Form(
+                autovalidateMode: state.showErrorMessages,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const BodyField(),
+                      const ColorField(),
+                      // list goes here
+                      const AddTodoTile(),
+                    ],
+                  ),
+                )),
+          );
         },
       ),
     );
